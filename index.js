62134689d7a3d95041f02032385f90013dd78f24
nyawira4400 <brenda.kinyua@student.moringaschool.com>
2022-08-24 18:36:57 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction (callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    return function namedFn() {
+        console.log("named function")
+    };
+}
+function returnsAnAnonymousFunction(){
+    return () => ("anonymous function")
+}
